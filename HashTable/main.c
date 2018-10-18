27c1e977130813fea1119ec8f06c86619ad20f69
dirk <dirkadin@gmail.com>
2018-10-18 16:31:30 -0500

    Initial Commit

---
+++ HashTable/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HashTable
+//
+//  Created by Dirk on 10/18/18.
+//  Copyright © 2018 Dirk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
